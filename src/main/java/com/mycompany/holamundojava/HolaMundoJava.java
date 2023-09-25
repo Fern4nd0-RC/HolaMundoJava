aea15457380aeabb28382f947e2a40c9e7deebc4
Fernando Romero de la Cruz <fernando.rdelacruz50@gmail.com>
2023-09-24 20:14:36 -0700

    ola mundo

--- src/main/java/com/mycompany/holamundojava/HolaMundoJava.java
+++ src/main/java/com/mycompany/holamundojava/HolaMundoJava.java
@@ -5,5 +5,6 @@ public class HolaMundoJava {
     public static void main(String[] args) {
         System.out.println("Hola mundo!\n");
         System.out.println("Putas");
+        System.out.println("\n Voy a dominar el mundo");
     }
 }
