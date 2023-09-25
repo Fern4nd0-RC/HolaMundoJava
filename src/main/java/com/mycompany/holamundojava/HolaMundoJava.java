8a93571ae8db1537d1a421a4fcf8f90912ea15ff
Fernando Romero de la Cruz <fernando.rdelacruz50@gmail.com>
2023-09-24 20:05:29 -0700

    Primer borrador

---
+++ src/main/java/com/mycompany/holamundojava/HolaMundoJava.java
@@ -0,0 +1,9 @@
+package com.mycompany.holamundojava;
+
+public class HolaMundoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!\n");
+        System.out.println("Putas");
+    }
+}
